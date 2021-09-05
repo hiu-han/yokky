78ee0ab82c8c1f7b8bf4e48113a050090a57c34b
ariados22 <73466106+hiu-han@users.noreply.github.com>
2021-09-06 02:24:27 +0900

    add today

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,41 @@
+const today = document.querySelector("#today");
 const clock = document.querySelector("#clock");
+const date = new Date();
+
+function getDate() {
+  const year = String(date.getFullYear());
+  const month = String(date.getMonth()).padStart(2, "0");
+  const toDate = String(date.getDate()).padStart(2, "0");
+  const day = date.getDay();
+  let dayString = "";
+  function getDateString(day) {
+    if (day === 0) {
+      dayString = "SUN";
+    } else if (day === 1) {
+      dayString = "MON";
+    } else if (day === 2) {
+      dayString = "TUE";
+    } else if (day === 3 ) {
+      dayString = "WED";
+    } else if (day === 4) {
+      dayString = "THU";
+    } else if (day === 5) {
+      dayString = "FRI";
+    } else {
+      dayString = "SAT";
+    }
+  }
+  getDateString(day);
+  today.innerText = `${year}. ${month}. ${toDate} ${dayString}`;
+}
 
 function getClock() {
-  const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
   clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
+getDate();
 getClock();
 setInterval(getClock, 1000);
