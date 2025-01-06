6c0435722282d1914ebece265ffff9377204fc46
Heejae Han <ariados104@gmail.com>
2025-01-06 13:38:02 +0900

    no message

--- MetWork/metrix/public/js/include.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-  $("#mainHeader").load("./include/mainHeader.html");
-  $('#header').load('./include/header.html');
-  $('#footer').load('./include/footer.html');
-})
--- README.md
+++
@@ -1,2 +0,0 @@
-# yokky
- momentum
