95d8d4c423dbbffeb39c4af25c68c65d29713a6b
Heejae Han <ariados104@gmail.com>
2025-01-12 15:48:32 +0900

    MetWork > gcgfPanelSurvey 업로드

--- MetWork/gcgfPanelSurvey/public/js/include.js
+++ MetWork/gcgfPanelSurvey/public/js/include.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
-  $('#header').load('/include/header.html');
-  $('#footer').load('/include/footer.html');
+  $('#header').load('include/header.html');
+  $('#footer').load('include/footer.html');
 });
